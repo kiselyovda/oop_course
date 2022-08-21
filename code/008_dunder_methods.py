b12b11f003b7616f79f19829df4f1415dc9eccee
Daniil Kiselyov <kiselyovda@ya.ru>
2022-08-21 16:22:22 +0300

    dunder methods

---
+++ code/008_dunder_methods.py
@@ -0,0 +1,18 @@
+class Data:
+    def __init__(self) -> None:
+        self.value = ''
+
+    def __get__(self, instance, owner):
+        print('__get__')
+        return self.value
+    
+    def __set__(self, instance, value):
+        print('__set__')
+        self.value = value
+
+
+class User:
+    name = Data()
+    surname = Data()
+
+test = User()
